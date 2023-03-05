ff9bd07126ad2e9527646df3fbb65337d4c39051
DianZhi97 <98074417+DianZhi97@users.noreply.github.com>
2023-03-05 01:26:32 -0500

    Change the userinput logic to make the app more user-frendly

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import streamlit_authenticator as stauth
 from streamlit_chat import message
 import yaml
 
+# config the page title and icon
 st.set_page_config(page_title="ChatGPT", page_icon='🤖')
 
+# use personalized css
 def load_css(filepath):
     with open(filepath, 'r') as fID:
         st.markdown(f'<style>{fID.read()}</style>', unsafe_allow_html=True)
 
 load_css('style/style.css')
 
+# load registered user info 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
 
+# config the auhenticator
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
@@ -23,6 +27,7 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
+# start the authenticator
 name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status == False:
@@ -30,8 +35,11 @@ if authentication_status == False:
 elif authentication_status == None:
     st.warning('Please enter your username and password')
 else:
+    # get OpenAI API key
     with open('openai_apikey.txt', 'r') as fID:
         openai.api_key = fID.readline()
+
+    # init session states
     if 'generated_response' not in st.session_state:
         st.session_state['generated_response'] = []
     if 'past_prompt' not in st.session_state:
@@ -40,14 +48,14 @@ else:
         st.session_state['conversation'] = [
             {'role': 'system', 'content': 'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.'}]
 
+    # reset the conversation history
     def reset_conversation():
-        st.session_state['input'] = ''
-        user_input = ''
         st.session_state['generated_response'] = []
         st.session_state['past_prompt'] = []
         st.session_state['conversation'] = [
             {'role': 'system', 'content': 'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.'}]
 
+    # takes a list of conversation and temperature and get response text
     def query(conversation, temperature):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", 
@@ -55,25 +63,15 @@ else:
             temperature=temperature)
         return response['choices'][0]['message']['content']
 
-    st.container()
+    # use form to gather user input
+    def get_input():
+        with st.form('get_text_input', clear_on_submit=True):
+            user_input = st.text_area('输入对话内容:', '')
+            submitted = st.form_submit_button('发送')
+        if submitted:
+            return user_input
 
-    col_left, col_right = st.columns([1,3])
-    with col_left:
-        st.title('ChatGPT')
-    with col_right:
-        st.markdown('#####')
-        st.image('https://deasilex.com/wp-content/uploads/2022/12/openai.webp', width=100)
-    
-    st.write('---')
-
-    col_left, col_right = st.columns([5,1])
-    with col_left:
-        st.write('本App直接调用了OpenAI的GPT-3.5 API [OpenAI ChatGPT 入口](https://chat.openai.com/)')
-    with col_right:
-        btn_pressed = st.button('清空对话', on_click=reset_conversation)
-    
-    user_input = st.text_input('输入对话(回车键发送):' ,'', key='input')
-    
+    # sidebar
     with st.sidebar:
         col_left, col_middle, col_right = st.columns([1, 10, 1])
         with col_middle:
@@ -83,6 +81,27 @@ else:
             temperature = st.slider('Temperature', min_value=0.0, max_value=2.0, value=0.5, step=0.05)
             st.write('Tips: 更高的Temperature会使ChatGPT的回答更加多样化')
 
+    # title + image
+    col_left, col_right = st.columns([1,3])
+    with col_left:
+        st.title('ChatGPT')
+    with col_right:
+        st.markdown('#####')
+        st.image('https://deasilex.com/wp-content/uploads/2022/12/openai.webp', width=100)
+    
+    st.write('---')
+
+    # description + clear button
+    col_left, col_right = st.columns([5,1])
+    with col_left:
+        st.write('本App直接调用了OpenAI的GPT-3.5 API [OpenAI ChatGPT 入口](https://chat.openai.com/)')
+    with col_right:
+        btn_pressed = st.button('清空对话', on_click=reset_conversation)
+    
+    # text input
+    user_input = get_input()
+
+    # format and send query to OpenAI API and get response
     if user_input:
         st.session_state.conversation.append({'role': 'user', 'content': user_input})
         output = query(st.session_state.conversation, temperature)
@@ -90,6 +109,7 @@ else:
         st.session_state.generated_response.append(output)
         st.session_state.conversation.append({'role': 'assistant', 'content': output})
 
+    # display the chat history
     if st.session_state['generated_response']:
         for i in range(len(st.session_state['generated_response'])-1, -1, -1):
             message(st.session_state['generated_response'][i], key=str(i), 
