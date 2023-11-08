27778706f881f07f9ee0fd7f96c777abe8bbc0ee
DianZhi97 <98074417+DianZhi97@users.noreply.github.com>
2023-11-08 12:56:20 -0500

    Change the chat loop and see if this fix the assistant not responding issue

--- main.py
+++ main.py
@@ -93,15 +93,16 @@ else:
         st.chat_message(msg.role).write(msg.content)
 
 
-    if prompt:= st.chat_input(disabled=False, key="chat_input"):
+    
+    if prompt := st.chat_input(disabled=False, key="chat_input"):
         st.chat_input(disabled=True, key="disabled_chat_input")
         st.session_state.messages.append(ChatMessage(role="user", content=prompt))
         st.chat_message("user").write(prompt)
 
-        while st.session_state.messages[-1].role != "assistant":
-            with st.chat_message("assistant"):
-                stream_handler = StreamHandler(st.empty())
-                llm = ChatOpenAI(openai_api_key=st.secrets['openai_apikey'], streaming=True, callbacks=[stream_handler])
-                response = llm([ChatMessage(role='system', content=custom_instructions)] + st.session_state.messages)
-                st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
+    if len(st.session_state.messages) > 0 and st.session_state.messages[-1].role != "assistant":
+        with st.chat_message("assistant"):
+            stream_handler = StreamHandler(st.empty())
+            llm = ChatOpenAI(openai_api_key=st.secrets['openai_apikey'], streaming=True, callbacks=[stream_handler])
+            response = llm([ChatMessage(role='system', content=custom_instructions)] + st.session_state.messages)
+            st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
         st.rerun()
